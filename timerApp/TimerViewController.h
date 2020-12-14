621ecd760bfd8e3f61d7297bf7d1feffead664a8
xiaokangyan <xiaokangyan@tencent.com>
2020-12-14 15:32:48 +0800

    修改工程配置
    将lifecycle管理换成appdelegate

---
+++ timerApp/TimerViewController.h
@@ -0,0 +1,16 @@
+//
+//  TimerViewController.h
+//  timerApp
+//
+//  Created by xiaokangyan on 2020/12/14.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TimerViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ timerApp/TimerViewController.m
@@ -0,0 +1,23 @@
+//
+//  TimerViewController.m
+//  timerApp
+//
+//  Created by xiaokangyan on 2020/12/14.
+//
+
+#import "TimerViewController.h"
+#import <Foundation/Foundation.h>
+
+@interface TimerViewController ()
+
+@end
+
+@implementation TimerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSLog(@"timer test");
+}
+
+@end
