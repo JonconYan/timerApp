5c85650c5bf50c2c2489f99b57614eb09254b10b
xiaokangyan <xiaokangyan@tencent.com>
2020-12-14 14:57:16 +0800

    Initial Commit

---
+++ timerApp/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  timerApp
+//
+//  Created by xiaokangyan on 2020/12/14.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ timerApp/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  timerApp
+//
+//  Created by xiaokangyan on 2020/12/14.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
